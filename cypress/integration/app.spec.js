309a964795800b843d6418b453cd59878ad2a9b1
Rita <ritinha.belo@hotmail.com>
2022-05-23 09:04:13 +0100

    change cypress tests
    change cypress tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe("App tests", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:8000");
+    cy.visit("http://localhost:8080");
   });
 
   it("should find title in page", () => {
@@ -13,79 +13,79 @@ describe("App tests", () => {
   it("should redirect to company page - 1", () => {
     cy.get("[data-testid=1]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/1");
+      expect(loc.href).to.eq("http://localhost:8080/company/1");
     });
     cy.get("[data-testid=number]").should("have.length", 6);
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
   it("should redirect to company page - 2", () => {
     cy.get("[data-testid=2]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/2");
+      expect(loc.href).to.eq("http://localhost:8080/company/2");
     });
     cy.get("[data-testid=number]").should("have.length", 7);
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
   it("should redirect to company page - 3", () => {
     cy.get("[data-testid=3]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/3");
+      expect(loc.href).to.eq("http://localhost:8080/company/3");
     });
     cy.get("[data-testid=number]").should("have.length", 0);
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
 
   it("should redirect to company page - 4", () => {
     cy.get("[data-testid=4]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/4");
+      expect(loc.href).to.eq("http://localhost:8080/company/4");
     });
     cy.get("[data-testid=number]").should("have.length", 2);
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
 
   it("should redirect to company page - 5", () => {
     cy.get("[data-testid=5]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/5");
+      expect(loc.href).to.eq("http://localhost:8080/company/5");
     });
     cy.get("[data-testid=number]").should("have.length", 2);
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
 
   it("should redirect to company page - 1 and to numbers page and back", () => {
     cy.get("[data-testid=1]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/1");
+      expect(loc.href).to.eq("http://localhost:8080/company/1");
     });
     cy.get("[data-testid=351910000000]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/numbers/351910000000");
+      expect(loc.href).to.eq("http://localhost:8080/numbers/351910000000");
     });
     cy.contains("351910000000");
     cy.contains("mobile");
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/company/1");
+      expect(loc.href).to.eq("http://localhost:8080/company/1");
     });
     cy.get("[data-testid=back]").click();
     cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:8000/");
+      expect(loc.href).to.eq("http://localhost:8080/");
     });
   });
 });
